2c02ffca523ba5881d611b91903b341118350b23
EC2 Default User <ec2-user@ip-172-31-45-130.ap-northeast-1.compute.internal>
2024-09-15 05:36:18 +0000

    CRUD機能完了？

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -10,18 +10,18 @@ class PostImagesController < ApplicationController
       redirect_to post_images_path(@post_image.id)
     else
       render :new
-    end 
+    end
   end
-  
+
   def edit
-    @post_image = PostImage(params[:id])
-  end 
-  
+    @post_image = PostImage.find(params[:id])
+  end
+
   def update
     post_image = PostImage.find(params[:id])
     post_image.update(post_image_params)
     redirect_to post_image_path(post_image.id)
-  end 
+  end
 
   def index
     @post_images = PostImage.all
